3ea749d06295232572382f73e0b8e00035f1b59a
Daniel Mendonça Acorroni <daniel.acorroni@hotmail.com>
2023-01-30 15:09:42 -0300

    feat: :sparkles: Add published checkbox

--- galeria/admin.py
+++ galeria/admin.py
@@ -5,5 +5,6 @@ class ListandoFotografias(admin.ModelAdmin):
   list_display = ('id', 'nome', 'categoria', 'legenda', 'descricao')
   list_display_links = ('id', 'nome')
   search_fields = ('nome',)
+  list_editable = ('publicada',)
 
 admin.site.register(Fotografia, ListandoFotografias)
--- galeria/models.py
+++ galeria/models.py
@@ -13,7 +13,7 @@ class Fotografia(models.Model):
     legenda = models.CharField(max_length=200, null=False, blank=False)
     categoria = models.CharField(max_length=100, choices=OPCOES_CATEGORIA, default='')
     descricao = models.TextField(null=False, blank=False)
-    foto = models.CharField(max_length=100, null=False, blank=False)
+    publicada = models.BooleanField(default=False)
 
     def __str__(self):
       return f'''Nome: {self.nome}
