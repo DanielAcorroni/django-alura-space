110f980ff0a96f25c56d8feaf9d6ffc24ff77c4b
Daniel Mendonça Acorroni <daniel.acorroni@hotmail.com>
2023-01-31 15:32:03 -0300

    feat: :sparkles: Create new app usuarios

--- setup/settings.py
+++ setup/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    'galeria.apps.GaleriaConfig',
+    "galeria.apps.GaleriaConfig",
+    "usuarios.apps.UsuariosConfig",
 ]
 
 MIDDLEWARE = [
