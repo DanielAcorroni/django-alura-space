3ca21793592f540f7dccd60c5b3e4bb5702e36bb
Daniel Mendonça Acorroni <daniel.acorroni@hotmail.com>
2023-02-02 19:50:54 -0300

    feat: :sparkles: Create form def for login

---
+++ usuarios/forms.py
@@ -0,0 +1,23 @@
+from django import forms
+
+
+class LoginForms(forms.Form):
+    nome_login = forms.CharField(
+        label="Nome de Login",
+        required=True,
+        max_length=100,
+        widget=forms.TextInput(
+            attrs={
+                "class": "form-control",
+                "placeholder": "Ex.: Daniel Acorroni",
+            }
+        ),
+    )
+    senha = forms.CharField(
+        label="Senha",
+        required=True,
+        max_length=70,
+        widget=forms.PasswordInput(
+            attrs={"class": "form-control", "placeholder": "Digite sua senha"}
+        ),
+    )
