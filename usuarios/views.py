c5225793b82a544f72391cdaf9b4a19fefc7bf46
Daniel Mendonça Acorroni <daniel.acorroni@hotmail.com>
2023-02-02 19:51:31 -0300

    feat: :sparkles: Create def in view.py for login

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from usuarios.forms import LoginForms, CadastroForms
+from django.contrib.auth.models import User
+from django.contrib import auth, messages
+
+
+def login(request):
+    form = LoginForms()
+
+    if request.method == "POST":
+        form = LoginForms(request.POST)
+
+        if form.is_valid():
+            nome = form["nome_login"].value()
+            senha = form["senha"].value()
+
+            usuario = auth.authenticate(request, username=nome, password=senha)
+
+            if usuario is not None:
+                auth.login(request, usuario)
+                messages.success(
+                    request, f"Login de {nome} efetuado com sucesso!"
+                )
+                return redirect("index")
+            else:
+                messages.error(request, "Usuário ou senha inexistentes!")
+                return redirect("login")
+
+    return render(request, "usuarios/login.html", {"form": form})
 
 # Create your views here.
