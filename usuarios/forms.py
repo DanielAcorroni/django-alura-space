381feed3671c1e0d19d8e9c1d6947a06c4e81247
Daniel Mendonça Acorroni <daniel.acorroni@hotmail.com>
2023-02-02 19:51:04 -0300

    feat: :sparkles: Create form def for sign-in

--- usuarios/forms.py
+++ usuarios/forms.py
@@ -21,3 +21,47 @@ class LoginForms(forms.Form):
             attrs={"class": "form-control", "placeholder": "Digite sua senha"}
         ),
     )
+
+
+class CadastroForms(forms.Form):
+    nome_cadastro = forms.CharField(
+        label="Nome de Login",
+        required=True,
+        max_length=100,
+        widget=forms.TextInput(
+            attrs={
+                "class": "form-control",
+                "placeholder": "Ex.: Daniel Acorroni",
+            }
+        ),
+    )
+    email = forms.EmailField(
+        label="Email",
+        required=True,
+        max_length=100,
+        widget=forms.EmailInput(
+            attrs={
+                "class": "form-control",
+                "placeholder": "danielacorroni@xpto.com",
+            }
+        ),
+    )
+    senha_1 = forms.CharField(
+        label="Senha",
+        required=True,
+        max_length=70,
+        widget=forms.PasswordInput(
+            attrs={"class": "form-control", "placeholder": "Digite sua senha"}
+        ),
+    )
+    senha_2 = forms.CharField(
+        label="Confirme sua senha",
+        required=True,
+        max_length=70,
+        widget=forms.PasswordInput(
+            attrs={
+                "class": "form-control",
+                "placeholder": "Digite sua senha novamente",
+            }
+        ),
+    )
